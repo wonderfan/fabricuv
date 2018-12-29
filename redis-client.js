ba62672fdbaf1a03b8e9f1393fd41242b0f1ed4d
fanjiahe <fanjiahe@ziggurat.cn>
2018-12-29 03:29:26 +0000

    redis client

---
+++ redis-client.js
@@ -0,0 +1,15 @@
+const RedisClustr = require('redis-clustr');
+
+var redis = new RedisClustr({
+  servers: [
+    {
+      host: '127.0.0.1',
+      port: 6379
+    }
+  ]
+});
+
+redis.set("abc","def");
+
+redis.quit();
+
